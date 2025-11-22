7d9b65e7b2619fec4e00ebd414ab8879f4781ba5
Mbali Msweli <mbalimsweli.nongalo@gmail.com>
2025-11-22 19:40:53 +0200

    update eda

--- 2.exploratory_data_analysis.sql
+++ 2.exploratory_data_analysis.sql
@@ -5,6 +5,19 @@ USE [Car_Sales_Analysis];
 SELECT *
 FROM [dbo].[car_sales_data ];
 
+--checking the nulls
+SELECT *
+FROM [dbo].[car_sales_data ]
+WHERE make IS NULL;
+
+--converting rows with nulls into Unknown
+UPDATE [dbo].[car_sales_data ]
+SET make = COALESCE(make, 'Unknown'),
+    model = COALESCE(make, 'Unknown'),
+    trim = COALESCE(make, 'Unknown'),
+    body = COALESCE(make, 'Unknown');
+
+
 --Checking how many different body type in this column
 SELECT DISTINCT(body)
 FROM [dbo].[car_sales_data ];
@@ -91,30 +104,85 @@ FROM [dbo].[car_sales_data ];
 
 ---Checking the maximum odometer
 SELECT MAX (odometer)
-FROM [dbo].[car_sales_data ]
+FROM [dbo].[car_sales_data ];
 
 --Counting the number of each transimission 
 SELECT DISTINCT (transmission), COUNT(*) AS Total_Transmission
 FROM [dbo].[car_sales_data ]
 GROUP BY transmission;
 
----Checking the minimum condition
+--- checking the minimum condition
 SELECT MIN (condition)
 FROM [dbo].[car_sales_data ];
 
----Checking the maximum condition
+--- checking the maximum condition
 SELECT MAX (condition)
 FROM [dbo].[car_sales_data ];
+
+--checking the different state in the dataset
+SELECT DISTINCT (state)
+FROM [dbo].[car_sales_data ];
+
+---checking the number of salesdate per state
+SELECT TOP 5 state, COUNT(saledate) AS Total_SalesDate_Per_State
+FROM [dbo].[car_sales_data ]
+GROUP BY state
+ORDER BY  COUNT(saledate) DESC;
 
 ---altering the table to add the new column called condition_bucket
 ALTER TABLE [dbo].[car_sales_data ]
 ADD condition_bucket VARCHAR (500);
 
----updating the new column to add data into a new column
+---updating the new column (condition_bucket) to add data into it
 UPDATE [dbo].[car_sales_data ]
 SET condition_bucket =
 	 CASE 
              WHEN condition between '1' AND '2' THEN 'Great Condition: 1 to 2'
              WHEN condition between '3' AND '4' THEN 'Normal Condition: 3 to 4'
              ELSE 'Poor Condition: 5'
+         END ;
+
+---altering the table to add the new column called year_model_bucket
+ALTER TABLE [dbo].[car_sales_data ]
+ADD year_model_bucket VARCHAR(500);
+
+---updating the new column (year_model_bucket) to add data into it
+UPDATE [dbo].[car_sales_data ]
+SET year_model_bucket =
+	 CASE 
+             WHEN condition between '1982' AND '1990' THEN 'Very Old Models: 1982 to 1990'
+             WHEN condition between '1991' AND '2000' THEN 'Old Models: 1991 to 2000'
+             WHEN condition between '2001' AND '2010' THEN 'Mid Age Models: 2001 to 2010'
+             ELSE 'Newer Models: 2011 to 2015'
+         END ;
+
+       
+---altering the table to add the new column called odometer_model_bucket
+ALTER TABLE [dbo].[car_sales_data ]
+ADD odometer_bucket VARCHAR(500);
+
+---updating the new column (odometer_bucket) to add data into it
+UPDATE [dbo].[car_sales_data ]
+SET odometer_bucket =
+	 CASE 
+             WHEN odometer between '1' AND '5000' THEN 'Low Mileage: 1 to 5000'
+             WHEN odometer between '50001' AND '150000' THEN 'Medium Mileage: 50001 to 150000'
+             WHEN odometer between '150001' AND '300000' THEN 'High Mileage: 150001 to 300000'
+             WHEN odometer between '300001' AND '600000' THEN 'Very High Mileage: 300001 to 600000'
+             ELSE 'Extreme Mileage: 600001 to 999999'
+         END ;
+
+---altering the table to add the new column called saleprice_model_bucket
+ALTER TABLE [dbo].[car_sales_data ]
+ADD saleprice_bucket VARCHAR(500);
+
+---updating the new column (saleprice_bucket) to add data into it
+UPDATE [dbo].[car_sales_data ]
+SET saleprice_bucket =
+	 CASE 
+             WHEN sellingprice between '1' AND '20000' THEN 'Low Price: 1 to 20000'
+             WHEN sellingprice between '20001' AND '60000' THEN 'Affordable: 20001 to 60000'
+             WHEN sellingprice between '60001' AND '120000' THEN 'Mid Range: 60001 to 120000'
+             WHEN sellingprice between '120001' AND '180000' THEN 'High Range: 120001 to 180000'
+             ELSE 'Premium: 180001 to 230000'
          END ;
