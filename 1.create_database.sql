d3b8b872bb111249787c7e14cba18c1dfe5b0797
Mbali Msweli <mbalimsweli.nongalo@gmail.com>
2025-12-13 17:34:49 +0200

    fix code

--- 1.create_database.sql
+++ 1.create_database.sql
@@ -1,1 +1,9 @@
-create database Car_Sales_Analysis
+
+---CREATE DATABASE: Car_Sales_Analysis
+
+IF NOT EXISTS (
+SELECT name FROM sys.databases 
+WHERE name = 'Car_Sales_Analysis')
+BEGIN
+    CREATE DATABASE Car_Sales_Analysis;
+END
